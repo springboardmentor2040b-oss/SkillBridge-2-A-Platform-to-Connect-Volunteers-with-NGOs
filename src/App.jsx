5bea2853da69ae42a775f33a93fec0eae5f536bd
AksharaKadapatri <aksharak692@gmail.com>
2026-02-15 12:01:52 +0530

    updated frontend

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";      
+import Login from "./pages/login";
+import Register from "./pages/register";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        {/* Home Page */}
+        <Route path="/" element={<Home />} />
+
+        {/* Login Page */}
+        <Route path="/login" element={<Login />} />
+
+        {/* Register Page */}
+        <Route path="/register" element={<Register />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
