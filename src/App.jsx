5c57f3e832e52cce80ad73b9024197cb34be8c07
manasaistanly <manasaistanly0@gmail.com>
2026-02-19 21:31:19 +0530

    Resolve merge conflict in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,8 @@ import Login from "./pages/login";
 import Register from "./pages/register";
 import Opportunities from "./pages/opportunities";
 import NGOs from "./pages/NGOs";
-<<<<<<< HEAD
 import Dashboard from "./pages/Dashboard";
 import CreateOpportunity from "./pages/CreateOpportunity";
-=======
->>>>>>> 9287dd5fbe5c08fc137238a83f113cc88e69204d
 
 function App() {
   return (
@@ -23,11 +20,8 @@ function App() {
 
         <Route path="/opportunities" element={<Opportunities />} />
         <Route path="/NGOs" element={<NGOs />} />
-<<<<<<< HEAD
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/create-opportunity" element={<CreateOpportunity />} />
-=======
->>>>>>> 9287dd5fbe5c08fc137238a83f113cc88e69204d
       </Routes>
     </BrowserRouter>
   );
